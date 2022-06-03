a8852cece30125142ed65d8774039cdaaa011255
maximoamsj <63797862+maximoamsj@users.noreply.github.com>
2022-06-03 09:13:45 -0300

    Update transounaglobo.py

--- transounaglobo.py
+++ transounaglobo.py
@@ -1,9 +1,6 @@
 #import modules
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from time import sleep
 import tweepy
 # keys
